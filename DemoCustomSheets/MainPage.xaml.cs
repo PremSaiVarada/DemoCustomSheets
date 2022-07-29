0796e58197be2dfa6b015bfe89bd9ef3d426427e
Prem Sai Varada <premsai.varada@websynergies.com>
2022-07-29 16:48:45 +0530

    demo for the verison 1.0.4

--- DemoCustomSheets/MainPage.xaml.cs
+++ DemoCustomSheets/MainPage.xaml.cs
@@ -1,24 +1,115 @@
-﻿namespace DemoCustomSheets;
+﻿using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using static DemoCustomSheets.MonekyApi;
+
+namespace DemoCustomSheets;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+    private async void LoadData()
+    {
+        try
+        {
+            incdicate.IsVisible=true;
+            MonkeyButton.IsVisible = Listbutton.IsVisible = false;
+            HttpClient httpClient = new HttpClient();
+            string RestUrl = @"https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/MonkeysApp/monkeydata.json";
+            var response = await httpClient.GetAsync(RestUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var Items = JsonConvert.DeserializeObject<ObservableCollection<MonkeyApi>>(content);
+                var rnd = new Random();
+                var randomMonkey = Items.OrderBy(item => rnd.Next());
+                ApiListViewName.ItemsSource = randomMonkey.Take(5).ToList();
+                incdicate.IsVisible = false;
+                MonkeyButton.IsVisible = Listbutton.IsVisible = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "Ok");
+        }
+
+    }
+    private async void Button_Clicked(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		await DefaultSheet.OpenSheet();
 	}
+
+	private async void Button_Clicked_1(object sender, EventArgs e)
+	{
+        CommonSheet.SheetHeight = 150;
+        CommonSheet.SheetWidth = 300;
+		await CommonSheet.OpenSheet();
+	}
+
+	private async void Button_Clicked_2(object sender, EventArgs e)
+	{
+        await CloseRemoved.OpenSheet();
+        LoadData();
+	}
+
+    private async void SheetClose(object sender, EventArgs e)
+    {
+        if (CloseRemoved.SheetVisible)
+            await CloseRemoved.CloseSheet();
+        if (sheetview.SheetVisible)
+            await sheetview.CloseSheet();
+    }
+
+    private async void Button_Clicked_3(object sender, EventArgs e)
+    {
+        CommonSheet.SheetHeight = 150;
+        CommonSheet.SheetWidth = 300;
+        await CommonSheet.OpenSheet();
+    }
+    protected override bool OnBackButtonPressed()
+    {
+        if (CommonSheet.SheetVisible)
+        {
+            CommonSheet.CloseSheet();
+            return true;
+        }
+        else
+            return base.OnBackButtonPressed();
+    }
+
+    private async void Button_Clicked_4(object sender, EventArgs e)
+    {
+        CommonSheet.SheetHeight = 150;
+        CommonSheet.SheetWidth = 300;
+        CommonSheet.BackClickClose = true;
+        CommonSheet.IsRoundRectangleVisible = false;
+        CommonSheet.SheetBackgroundColor = Colors.White;
+        await CommonSheet.OpenSheet();
+    }
+
+    private async void Button_Clicked_5(object sender, EventArgs e)
+    {
+        CommonSheet.SheetHeight = 150;
+        CommonSheet.SheetWidth = 300;
+        CommonSheet.IsRoundRectangleVisible = false;
+        CommonSheet.SheetBackgroundColor = Colors.White;
+        await CommonSheet.OpenSheet();
+    }
+
+    private async void Button_Clicked_6(object sender, EventArgs e)
+    {
+        await sheetview.OpenSheet();
+    }
+
+    private async void Button_Clicked_7(object sender, EventArgs e)
+    {
+        CommonSheet.SheetHeight = 150;
+        CommonSheet.SheetWidth = 300;
+        CommonSheet.SheetBackgroundColor = Colors.Pink;
+        await CommonSheet.OpenSheet();
+    }
 }
 
---
+++ DemoCustomSheets/MonekyApi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DemoCustomSheets
+{
+    internal class MonekyApi
+    {
+        public class MonkeyApi
+        {
+            public string Name { get; set; }
+            public string Location { get; set; }
+            public string Details { get; set; }
+            public string Image { get; set; }
+            public int Population { get; set; }
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+            [JsonIgnore]
+            public bool ButtonEnabel => IsAllFill();
+
+            private bool IsAllFill()
+            {
+                return !string.IsNullOrWhiteSpace(Latitude.ToString()) && !string.IsNullOrWhiteSpace(Longitude.ToString());
+            }
+        }
+    }
+}
